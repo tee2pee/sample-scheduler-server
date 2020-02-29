1520377cbd395ad480547051cc61e5938e45548d
tee2pee <tee2pee4dev@gmail.com>
2020-02-29 15:41:06 +0900

    ユーザーモデル追加

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  name: 'Test User',
+  email: 'test@example.com',
+  password: 'passw0rd',
+  password_confirmation: 'passw0rd',
+  deleted: false
+)
