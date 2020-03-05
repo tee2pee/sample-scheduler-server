a3d69c10736b903857d50e9cfe70a9c6d461f328
tee2pee <tee2pee4dev@gmail.com>
2020-03-05 23:53:56 +0900

    migrate

--- app/db/migrate/20200301000000_init.rb
+++ app/db/migrate/20200301000000_init.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
+
     create_table :users do |t|
       t.string :name
       t.string :email
@@ -9,5 +10,34 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps
     end
     add_index :users, :email, unique: true
+
+    create_table :calendars do |t|
+      t.string :title
+      t.boolean :deleted
+
+      t.timestamps
+    end
+
+    create_table :schedules do |t|
+      t.belongs_to :calendar
+      t.datetime :fr_datetime
+      t.datetime :to_datetime
+      t.string :title
+      t.boolean :deleted
+
+      t.timestamps
+    end
+    add_index :schedules, [:calendar]
+
+    create_table :relationships do |t|
+      t.belongs_to :user
+      t.belongs_to :calendar
+      t.boolean :deleted
+
+      t.timestamps
+    end
+    add_index :relationships, [:user]
+    add_index :relationships, [:user, :calendar], unique: true
+
   end
 end
