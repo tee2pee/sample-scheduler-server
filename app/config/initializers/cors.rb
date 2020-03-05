69c5aea8278f13e1ef7a21fa28b1f93a853d50db
tee2pee <tee2pee4dev@gmail.com>
2020-03-05 23:16:23 +0900

    エンドポイント設定

--- app/config/initializers/cors.rb
+++ app/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3030'
+    origins ENV.fetch('CLIENT_ORIGIN', 'tee2pee-sample-scheduler.netlify.com')
 
     resource '*',
       headers: :any,
